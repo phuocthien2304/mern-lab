185532a66e3bfb57b5e589ca193c7d8cd17c6a45
phuocthien2304 <162446480+phuocthien2304@users.noreply.github.com>
2025-09-27 15:18:29 +0700

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -7,8 +7,22 @@ import todoRouter from "./routes/todo.routes.js";
 
 dotenv.config();
 
-const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+const app = express();const allowedOrigins = [
+  process.env.CLIENT_URL,         // vercel
+  "http://localhost:5173"         // local dev
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
